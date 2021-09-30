e6fc2f6b5748bd0a774c27758f0ec3b8052531ee
Jeremy Le <jeremyle@live.com>
2021-09-30 22:03:58 +1000

    add cls triadProb function

--- randomclass.py
+++ randomclass.py
@@ -7,11 +7,27 @@ class GenerateRandom:
 
     def __init__(self):
         self.data = ''
+        self.split = None
+        self.counts = None
+        self.probs = None
         self.bank = 1000
 
     def inputFilter(self, inputs):
         self.data += ''.join(x for x in inputs if x in '01')
         return self.data
+
+    def triadProb(self):
+        """Takes input from user, split into four character slices, counts them
+        and then calculate probability for 0 or 1 following a triad into a dictionary."""
+        triads = ['000', '001', '010', '011', '100', '101', '110', '111']
+        self.split = [self.data[i: i + 4]
+                      for i in range(0, len(self.data) - 3)]
+        self.counts = {key: [self.data.count(
+            key + '0'), self.data.count(key + '1')] for key in triads}
+        self.probs = {key: (value[0] + 0.0001) / (value[0] + value[1] + 0.0001)
+                      for key, value in self.counts.items()}
+
+        print(self.split)
 
 
 game = GenerateRandom()
@@ -22,17 +38,9 @@ while len(game.data) < 10:
         f'Current data length is {len(game.data)}, {10 - len(game.data)} symbols left')
     game.inputFilter(input('Print a random string containing 0 or 1: '))
 
-print(f'Final data string:\n{game.data}\n')
+print(f'Final data string:\n{game.data}')
 
-# print(len(game.data))
-
-# game.inputfilter(input('Print a random string containing 0 or 1: '))
-
-# print(game.data)
-
-# game.inputfilter(input('Print a random string containing 0 or 1: '))
-# print(game.data)
-
+game.triadProb()
 
 # print(f'Final data string:\n{final_data}\n')
 # print(f'Current data length is {len(self.final_data)}, {10 - len(self.final_data)} symbols left')
